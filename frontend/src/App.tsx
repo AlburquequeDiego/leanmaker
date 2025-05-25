f2a48067aef0a2a2bc7c70074fb0817852ae9a7a
diego alburqueque <113200683+AlburquequeDiego@users.noreply.github.com>
2025-05-25 10:52:47 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          esta sera el front en para el sistema <code> LEANMAKER </code>
+          esta sera el front end para el sistema <code> LEANMAKER </code>
         </p>
         <a
           className="App-link"
